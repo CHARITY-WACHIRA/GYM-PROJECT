82362e6af549024bcd254834386d36f1d8aae310
user <+>
2023-07-07 06:46:39 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("db.json")
+fetch("db.json") // add your server
     .then(response => response.json())
     .then(data => {
       // Getting schedule container
@@ -8,14 +8,13 @@ fetch("db.json")
 
       // Function for updating schedule based on day 
       const updateSchedule = (day) => {
-        console.log("Day selected is: ", day); 
         const classData = data.find(item => item.day === day); 
         
+        // Changing the html content
         scheduleContainer.querySelector('h3').textContent = `Our ${classData.class} Classes`; 
         scheduleContainer.querySelector('p:nth-of-type(1)').textContent = `Day: ${classData.day}`;
         scheduleContainer.querySelector('p:nth-of-type(2)').textContent = `Class: ${classData.class}`;
         scheduleContainer.querySelector('p:nth-of-type(3)').textContent = `Time: ${classData.time}`;
-
       }
       
       // Adding event listeners to the buttons 
@@ -25,6 +24,7 @@ fetch("db.json")
             updateSchedule(selectedDay); 
         }); 
       }); 
+      
     })
     .catch(error => {
         console.log('Error fetching data:', error);
